b8dfc005fc927ffd9c30a1087acb47965f19cfd1
quannh <quannh@runsystem.net>
2023-09-14 17:45:54 +0700

    update

--- resources/assets/data/data.ts
+++ resources/assets/data/data.ts
@@ -1,19 +1,32 @@
 export const data = {
     "tree":
-        [{
-            "x": 100,
-            "y": 20,
-            "id": "ff663b16-6d48-7712-412c-4f1df81eb15c",
-            "name": "Catherine",
-            "surname": "of Valois",
-            "sex": "f",
-            "description": "Princess of France\r\n1401 - 1437",
-            "isNode": true,
-            "relationships": [{
-                "partnerId": "2279a9a9-fd2a-7d9f-2463-305f57faabd3",
-                "direction": "to",
-                "children": ["824fa647-8012-a425-5615-d30d9db62ca2"]
+        [
+            {
+                "x": 100,
+                "y": 20,
+                "id": "ff663b16-6d48-7712-412c-4f1df81eb15c",
+                "name": "Catherine",
+                "surname": "of Valois",
+                "sex": "f",
+                "description": "Princess of France\r\n1401 - 1437",
+                "isNode": true
+            },
+
+            {
+                "x": 400,
+                "y": 20,
+                "id": "2279a9a9-fd2a-7d9f-2463-305f57faabd3",
+                "name": "Owen",
+                "surname": "Tudor",
+                "sex": "m",
+                "description": "executed 1461",
+                "isNode": true,
+                "relationships": [{
+                        "partnerId": "ff663b16-6d48-7712-412c-4f1df81eb15c",
+                        "direction": "from",
+                        "children": ["824fa647-8012-a425-5615-d30d9db62ca2"]
+                    }
+                ]
             }
-            ]
-        }]
+        ]
 };
